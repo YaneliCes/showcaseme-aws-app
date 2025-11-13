0c2e579a3bf2dad545677a18d2a163cd0626c7b3
yc73 <yc73@njit.edu>
2025-11-13 03:11:54 -0500

    Update connection settings

--- frontend/showcaseme/src/App.jsx
+++ frontend/showcaseme/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState("Loading...");
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/hello")
+    fetch("http://10.0.2.56:3000/api/hello")
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(() => setMessage("Backend not reachable"));
